734b40352cba8e9189cb7e4358e7a314bcae4dc8
BuildTools <zan.oberstar@gmail.com>
2021-11-28 15:06:11 +0100

    ImGui example

--- Physics Fun/Application.cpp
+++ Physics Fun/Application.cpp
@@ -2,6 +2,10 @@
 #include <glad/gl.h>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+
 Application::Application(unsigned int width, unsigned int height, GLFWwindow* window) 
 	: m_Width(width), m_Height(height), m_Window(window) {
 
@@ -10,10 +14,18 @@ Application::Application(unsigned int width, unsigned int height, GLFWwindow* wi
 	m_ResManager.addShader("basic", new Shader("shaders/basic-vert.glsl", "shaders/basic-frag.glsl"));
 	m_ResManager.getShader("basic")->setMat4("proj", m_Projection);
 	m_ResManager.getShader("basic")->setMat4("view", glm::mat4(1.0f));
+
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(m_Window, true);
+	ImGui_ImplOpenGL3_Init("#version 330 core");
 }
 
 Application::~Application() {
-	
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 }
 
 void Application::handleEvents() {
@@ -31,6 +43,26 @@ void Application::update() {
 
 void Application::render() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// imgui begin frame
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::ShowDemoWindow();
+
+	static glm::vec4 color;
+	static float val;
+	if (ImGui::Begin("Test window")) {
+		if(ImGui::ColorEdit4("ClearColor", &color[0]))
+			glClearColor(color.r, color.g, color.b, color.a);
+		ImGui::SliderFloat("Test", &val, 0.0f, 1.0f);
+	}
+	ImGui::End();
+
+	// imgui render
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 	glfwSwapBuffers(m_Window);
 	glfwPollEvents();
