946f472e356a019c1414ce185b8b752184962718
Žan Oberstar <zan.oberstar@gmail.com>
2021-11-30 21:44:02 +0100

    Added mesh class

---
+++ Physics Fun/Mesh.cpp
@@ -0,0 +1,44 @@
+#include "Mesh.h"
+#include <glad/gl.h>
+
+Mesh::Mesh(std::vector<Vertex> vertexes, std::vector<unsigned int> indices) 
+	: m_Vertexes(vertexes), m_Indices(indices) {
+
+	glGenVertexArrays(1, &m_Vao);
+	glBindVertexArray(m_Vao);
+
+	glGenBuffers(1, &m_Vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, m_Vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertexes.size(), &vertexes[0], GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(6 * sizeof(float)));
+
+	glGenBuffers(1, &m_Ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_Ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned) * indices.size(), &indices[0], GL_STATIC_DRAW);
+
+	glBindVertexArray(0);
+}
+
+Mesh::~Mesh() {
+	glDeleteBuffers(1, &m_Ebo);
+	glDeleteBuffers(1, &m_Vbo);
+	glDeleteVertexArrays(1, &m_Vao);
+}
+
+void Mesh::render(Shader& shader) {
+	/*shader.setVec3("diffuseColor", material.diffuseColor);
+	shader.setVec3("specularColor", material.specularColor);
+	shader.setInt("diffuseTex", 0);
+	material.diffuseTex.bind(0);
+	shader.setInt("specularTex", 1);
+	material.specularTex.bind(1);*/
+
+	glBindVertexArray(m_Vao);
+	glDrawElements(GL_TRIANGLES, m_Indices.size(), GL_UNSIGNED_INT, NULL);
+	glBindVertexArray(0);
+}
---
+++ Physics Fun/Mesh.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include <glm/glm.hpp>
+
+#include "Shader.h"
+
+struct Vertex {
+	glm::vec3 position;
+	glm::vec3 normal;
+	glm::vec2 texcoord;
+};
+
+class Mesh {
+private:
+	std::vector<Vertex> m_Vertexes;
+	std::vector<unsigned int> m_Indices;
+
+	unsigned int m_Vao;
+	unsigned int m_Vbo;
+	unsigned int m_Ebo;
+public:
+	Mesh(std::vector<Vertex> vertexes,  std::vector<unsigned int> indices);
+	~Mesh();
+
+	void render(Shader& shader);
+};
+
